5f005680e5fc4dea4d20eb15e4988c9f6c3bb361
L-Luca <99327722+L-Luca6028@users.noreply.github.com>
2024-03-07 17:51:36 +0100

    Upload Models

---
+++ Models/Zutat.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Speisekarte.Models
+{
+    public class Zutat
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]        
+        public string Beschreibung { get; set; } = string.Empty;
+
+        [MaxLength(5), MinLength(2, ErrorMessage = "Minimum Length must be 2")] // Werden Data Annotations genannt
+        public string Einheit { get; set; } = string.Empty;
+        public decimal Menge { get; set; }
+        public Speise? Speise { get; set; } // Navigationproperty
+        public int? SpeiseId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Menge} {Einheit} {Beschreibung}";
+        }
+    }
+}
